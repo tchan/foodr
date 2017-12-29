65b7dc99aa473212175edd6b3b45da9784a71717
Travis Chan <travismchan@gmail.com>
2017-12-29 23:10:24 +1100

    Add shit card closing ability

--- app/components/base-card/component.js
+++ app/components/base-card/component.js
@@ -2,6 +2,10 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['base-card'],
+  show: true,
   actions: {
+    toggleAccordion() {
+      this.toggleProperty('show');
+    }
   }
 });
