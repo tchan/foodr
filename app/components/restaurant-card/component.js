ac74f200c8405fa02f455a6db2026f03ae91d059
Travis Chan <travismchan@gmail.com>
2017-12-30 17:23:57 +1100

    More card styling

--- app/components/restaurant-card/component.js
+++ app/components/restaurant-card/component.js
@@ -1,8 +1,16 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   elementId: 'restaurant-card',
-
+  title: computed('restaurantName', function () {
+    let restaurantName = this.get('restaurantName');
+    return `You last visited ${restaurantName}`;
+  }),
+  restaurantName: computed('restaurants', function() {
+    let restaurant = this.get('restaurants').objectAt(0);
+    return restaurant.get('name');
+  }),
   actions: {
   }
 });
