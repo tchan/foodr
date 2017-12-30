59fff3db8b74030c7feaf6ac1e3cee81fd0f7307
Travis Chan <travismchan@gmail.com>
2017-12-31 00:08:23 +1100

    Use computed filter macro and adjust styling

--- app/components/orders-disliked-card/component.js
+++ app/components/orders-disliked-card/component.js
@@ -1,8 +1,27 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   elementId: 'orders-disliked-card',
+  store: service(),
 
+  dislikedOrders: computed.filterBy('restaurants.orders', 'liked', false),
   actions: {
+    addFood(value) {
+      let restaurant = this.get('restaurants');
+      let foodOrder = this.get('store').createRecord('order', { name: value, liked: false });
+      restaurant.get('orders').then((orders) => {
+        orders.addObject(foodOrder);
+        restaurant.save().then(() => {
+          foodOrder.save();
+          this.set('name', null);
+        })
+      })
+    },
+
+    deleteFood(order) {
+      order.destroyRecord();
+    }
   }
 });
--- app/components/orders-liked-card/component.js
+++ app/components/orders-liked-card/component.js
@@ -1,18 +1,21 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
   elementId: 'orders-liked-card',
   store: service(),
 
+  likedOrders: computed.filterBy('restaurants.orders', 'liked', true),
   actions: {
     addFood(value) {
       let restaurant = this.get('restaurants');
-      let foodOrder = this.get('store').createRecord('order', { name: value });
+      let foodOrder = this.get('store').createRecord('order', { name: value, liked: true });
       restaurant.get('orders').then((orders) => {
         orders.addObject(foodOrder);
         restaurant.save().then(() => {
           foodOrder.save();
+          this.set('name', null);
         })
       })
     },
